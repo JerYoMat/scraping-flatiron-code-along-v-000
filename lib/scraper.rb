b87b37273f71947786697d531c9c422a46c83909
Jeremy Emata <jeremy.emata@gmail.com>
2018-10-09 21:47:57 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,6 +22,9 @@ class Scraper
   end 
   
   def get_courses 
+    html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+    doc = Nokogiri::HTML(html)
+    
   end 
 end
 
