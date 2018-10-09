c66a315ba1e33cc25fc91b86b9bb26af3d906721
Jeremy Emata <jeremy.emata@gmail.com>
2018-10-09 21:51:48 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
-
+require 'pry'
 require_relative './course.rb'
 
 class Scraper
@@ -24,7 +24,7 @@ class Scraper
   def get_courses 
     html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
     doc = Nokogiri::HTML(html)
-    doc.css(".h2").text 
+   new_doc = doc.css(".h2").text 
   end 
 end
 
